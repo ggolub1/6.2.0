9ced6878d0644060b2f0f96aa1c57755768ea742
ggolub1 <42158729+ggolub1@users.noreply.github.com>
2018-11-13 00:13:05 -0500

    Add files via upload

---
+++ 11_12_sqlsession.sql
@@ -0,0 +1,14 @@
+=== 11_12_sqlsession.sql
+select * from t1
+select * from [x201\i1].db11.dbo.t1
+
+i1 4,4
+sp_help t1
+
+select * from [x201\i1].db11.dbo.t1
+[x201\i1].db11.dbo.i1 6,6
+
+[x201\i1].db11.dbo.i2 7,7
+
+select * from t2
+select * from [x201\i1].db11.dbo.t2
---
+++ working.sql
@@ -0,0 +1,10 @@
+select * from [HumanResources].[Employee1]
+
+select t.text from sys.dm_exec_query_stats s
+cross apply sys.dm_exec_sql_text(s.sql_handle) t
+where t.text like '%Employee%'
+
+dbcc FREEPROCCACHE()
+
+
+truncate table [HumanResources].[Employee1]
