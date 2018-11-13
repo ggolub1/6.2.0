5e92e1fa41bc34e9c9ed1f70b17b3535fcc23d3d
ggolub1 <42158729+ggolub1@users.noreply.github.com>
2018-11-13 00:15:01 -0500

    Update working.sql

--- working.sql
+++ working.sql
@@ -1,4 +1,6 @@
+declare @cnt int
 select * from [HumanResources].[Employee1]
+select @cnt=@@rowcount
 
 select t.text from sys.dm_exec_query_stats s
 cross apply sys.dm_exec_sql_text(s.sql_handle) t
